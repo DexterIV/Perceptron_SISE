5f80df1a0749d39ff7102ac687d0fb837a615ded
DexterIV <wiedzmenpl@gmail.com>
2019-01-10 20:01:49 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# IAD2_PERCEPTRON_WIELOWARSTWOWY
+Zadanie 2
+## A training set http://www.pjreddie.com/media/files/mnist_train.csv
+## A test set http://www.pjreddie.com/media/files/mnist_test.csv
---
+++ Testing/SISE.py
@@ -0,0 +1,77 @@
+import random
+import numpy
+import math
+from library import funs
+from library.funs import parameters_as_string, print_plot, calculate_results_table
+
+from mlp.NeuralNetwork import NeuralNetwork
+
+
+def normalize(to_normalize):
+    vmin = min(to_normalize)
+    vmax = max(to_normalize)
+    normalized = []
+    for var in to_normalize:
+        normalized.append((var - vmin) / (vmax - vmin))
+    return normalized
+
+
+def denormalize(to_normalize, normalized):
+    vmin = min(to_normalize)
+    vmax = max(to_normalize)
+    denormalized = []
+    for var in normalized:
+        denormalized.append((var * (vmax - vmin)) + vmin)
+    return denormalized
+
+
+def task_SISE():
+    epochs = 200
+    hidden_nodes = 5
+    learning_rate = 0.2
+    momentum = 0.9
+    bias = True
+    network = NeuralNetwork(input_nodes=1, hidden_nodes=hidden_nodes, output_nodes=1,
+                            learning_rate=learning_rate,
+                            momentum=momentum, bias=bias, epochs=epochs)
+
+    input_list = list(range(1, 100))
+    output = []
+    for var in input_list:
+        output.append(math.sqrt(var))
+
+    c = list(zip(input_list, output))
+
+    # shuffle in the same way
+    random.shuffle(c)
+
+    input_list, output = zip(*c)
+
+    input_norm = normalize(input_list)
+    output_norm = normalize(output)
+    for e in range(1000):
+        for i in range(len(input_norm)):
+            network.train_manual_epochs([input_norm[i]], [output_norm[i]], e)  # inputs need to be arrays
+
+    fin = []
+    query_list = []
+    for i in range(len(input_norm)):
+        query_list.append(input_norm[i])
+        fin.append(network.query([query_list[i]]))
+
+    numpy.set_printoptions(suppress=True)  # avoid e-05 notation
+    print(output)
+
+    results = denormalize(output,fin)
+    for var in results:
+        print(var)
+
+    result_tab = numpy.zeros(shape=(len(input_norm), len(input_norm)))
+
+    for i in range(len(fin)):
+        result_tab[numpy.argmax(query_list[i])][numpy.argmax(fin[i])] += 1
+
+    print('Ex 1 results table')
+    print(result_tab)
+    parameters = parameters_as_string(hidden_nodes, learning_rate, momentum, epochs, bias)
+    funs.print_plot(network.sampling_iteration, network.errors_for_plot, 'Ex1 error plot ' + parameters)
